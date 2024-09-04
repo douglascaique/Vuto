9765cb7ada1aacc373acd2a81673abda694298b2
Douglas Caíque <douglascaique@hotmail.com>
2024-09-03 23:48:47 -0300

    v1.0

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function toggleMenu() {
     var menu = document.querySelector('.menu');
     menu.classList.toggle('show');
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Adiciona um atraso de 3 segundos antes de remover a classe 'loading'
+    setTimeout(function() {
+        document.body.classList.remove('loading');
+    }, 2000); // 3000 milissegundos = 3 segundos
+});
+
+// Adiciona a classe 'loading' ao body quando a página começa a carregar
+document.body.classList.add('loading');
